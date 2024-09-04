bd8603369d6f88df8d5d5ee6a42ea99833cc7f7f
Samuel Aroney <samuel.aroney@outlook.com>
2024-09-04 15:37:17 +1000

    use proper metapackage

--- compile_and_test_install_methods.bash
+++ compile_and_test_install_methods.bash
@@ -17,10 +17,12 @@ docker build --no-cache --progress=plain -f Dockerfile.pypi . &> pypi.build.log
 
 # Test docker install. Easier here than within a Dockerfile
 echo "Testing docker install .."
+git clone https://github.com/AroneyS/binchicken bc && cd bc && git checkout vBINCHICKEN_VERSION && cd ..
 chmod g+rw . # Docker needs this to write to the mounted volume - diamond uses it as a temp directory
-bash -c "docker pull samuelaroney/binchicken:$BINCHICKEN_VERSION && docker run -v `pwd`:/data samuelaroney/binchicken:$BINCHICKEN_VERSION coassemble --forward bc/test/data/sample_1.1.fq bc/test/data/sample_2.1.fq bc/test/data/sample_3.1.fq --reverse bc/test/data/sample_1.2.fq bc/test/data/sample_2.2.fq bc/test/data/sample_3.2.fq --genomes bc/test/data/GB_GCA_013286235.1.fna --singlem-metapackage bc/test/data/singlem_metapackage.smpkg --assemble-unmapped --unmapping-max-identity 99 --unmapping-max-alignment 90 --prodigal-meta --output test_docker" &> docker.build.log
+rm -rf test_docker
+bash -c "docker pull samuelaroney/binchicken:$BINCHICKEN_VERSION && docker run -v $(pwd):/data samuelaroney/binchicken:$BINCHICKEN_VERSION coassemble --forward bc/test/data/sample_1.1.fq bc/test/data/sample_2.1.fq bc/test/data/sample_3.1.fq --reverse bc/test/data/sample_1.2.fq bc/test/data/sample_2.2.fq bc/test/data/sample_3.2.fq --genomes bc/test/data/GB_GCA_013286235.1.fna --assemble-unmapped --unmapping-max-identity 99 --unmapping-max-alignment 90 --prodigal-meta --output /data/test_docker" &> docker.build.log
 
 # Test apptainer install. Too annoying to run this within a Dockerfile and simple enough here.
 echo "Testing apptainer install .."
 rm -f binchicken_$BINCHICKEN_VERSION.sif
-bash -c 'apptainer pull docker://samuelaroney/binchicken:$BINCHICKEN_VERSION && apptainer run --cleanenv -B `pwd`:`pwd` binchicken_$BINCHICKEN_VERSION.sif coassemble --forward bc/test/data/sample_1.1.fq bc/test/data/sample_2.1.fq bc/test/data/sample_3.1.fq --reverse bc/test/data/sample_1.2.fq bc/test/data/sample_2.2.fq bc/test/data/sample_3.2.fq --genomes bc/test/data/GB_GCA_013286235.1.fna --singlem-metapackage bc/test/data/singlem_metapackage.smpkg --assemble-unmapped --unmapping-max-identity 99 --unmapping-max-alignment 90 --prodigal-meta --output test_apptainer' > apptainer.build.stdout.log 2> apptainer.build.stderr.log
+bash -c 'singularity pull docker://samuelaroney/binchicken:$BINCHICKEN_VERSION && singularity run --cleanenv -B $(pwd) binchicken_$BINCHICKEN_VERSION.sif coassemble --forward $(pwd)/bc/test/data/sample_1.1.fq $(pwd)/bc/test/data/sample_2.1.fq $(pwd)/bc/test/data/sample_3.1.fq --reverse $(pwd)/bc/test/data/sample_1.2.fq $(pwd)/bc/test/data/sample_2.2.fq $(pwd)/bc/test/data/sample_3.2.fq --genomes $(pwd)/bc/test/data/GB_GCA_013286235.1.fna --assemble-unmapped --unmapping-max-identity 99 --unmapping-max-alignment 90 --prodigal-meta --output $(pwd)/test_singularity' > singularity.build.stdout.log 2> singularity.build.stderr.log
